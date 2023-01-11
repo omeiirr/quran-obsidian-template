5a7c96035660b87ed2690a34777e0e317441a91a
Omeir <omeirf.02@gmail.com>
2023-01-11 17:03:43 +0530

    define template: NamesOfAllahTemplate

---
+++ templates/NamesOfAllahTemplate.go
@@ -0,0 +1,7 @@
+package templates
+
+var NamesOfAllahTemplate = "\n\n> And to Allah belong the best names, so invoke Him by them. (Quran [[7-180]])" +
+	"\n\n> Allah - there is no deity except Him. To Him belong the best names. (Quran [[20:8]])" +
+	"\n\n> He is Allah, the Creator, the Inventor, the Fashioner; to Him belong the best names. (Quran [[59:24]])" +
+	"\n\n| Name | Arabic | Meaning | Info | Reference |" +
+	"\n| --- | --- | --- | --- | --- |"

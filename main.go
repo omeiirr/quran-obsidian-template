06de00755a3632132996d6559199909c17682b97
Omeir <omeirf.02@gmail.com>
2023-01-22 20:34:04 +0530

    embed files for functions separately

--- main.go
+++ main.go
@@ -11,15 +11,20 @@ import (
 // Embedded file being passed from main.go
 // Embedding inside modules not suported as of go v1.18.1
 
+//go:embed data/ontology-concepts.json
+var embeddedFile_concepts []byte
+
 //go:embed data/asma-ul-husna.json
-var embedded_file []byte
+var embeddedFile_AllahNames []byte
 
 func main() {
 	// To measure execution time
 	startTime := time.Now()
 
 	functions.GenerateFolderStructure()
+	functions.GenerateNamesOfAllah(embeddedFile_AllahNames)
 	functions.GenerateQuran(1, 114)
+	functions.GenerateOntology(embeddedFile_concepts)
 
 	elapsedTime := time.Since(startTime)
 	fmt.Printf("\n\nAlhamdulillah! Operation completed in %s \nHappy learning.\n", elapsedTime)

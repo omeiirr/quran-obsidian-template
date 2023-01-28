4803cdad4c3503357219497fa7da4271361482b7
Omeir <omeirf.02@gmail.com>
2023-01-28 13:44:58 +0530

    update GenerateOntology acc to new structure

--- functions/GenerateOntology.go
+++ functions/GenerateOntology.go
@@ -6,15 +6,15 @@ import (
 	"log"
 
 	"os"
-	"path/filepath"
 	"strings"
 
+	"github.com/omeiirr/quran-obsidian-template/models"
 	"github.com/omeiirr/quran-obsidian-template/templates"
 )
 
 func GenerateOntology(workingDir string, embedded_file []byte) {
 
-	var concepts []string
+	var concepts []models.Concept
 
 	err := json.Unmarshal(embedded_file, &concepts)
 	if err != nil {
@@ -24,26 +24,24 @@ func GenerateOntology(workingDir string, embedded_file []byte) {
 	for _, concept := range concepts {
 
 		// extract the directory path
-		dir := filepath.Dir(concept)
+		// dir := filepath.Dir(concept)
+		// fmt.Printf("\n dir: %s ", dir)
 
 		// create the directory, including any necessary parent directories
-		err := os.MkdirAll(fmt.Sprintf("%s/%s", workingDir, dir), os.ModePerm)
+		err := os.MkdirAll(fmt.Sprintf("%s/%s", workingDir, concept.Path), os.ModePerm)
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		conceptName := strings.Split(filepath.Base(concept), ".md")[0]
-		urlKeyword := strings.Split(conceptName, " (")[0]
-
 		// convert conceptName to lowercase, then replace ALL occurences of " " with "-"
 		// in accordance with the URLs of `corpus.quran.com`
-		externalUrl := fmt.Sprintf("https://corpus.quran.com/concept.jsp?id=%s", strings.Replace(strings.ToLower(urlKeyword), " ", "-", -1))
+		externalUrl := fmt.Sprintf("https://corpus.quran.com/concept.jsp?id=%s", strings.Replace(strings.ToLower(concept.ConceptEn), " ", "-", -1))
 
 		// define the content for each concept and insert into the template
 		content := []byte(
-			fmt.Sprintf(templates.OntologyConceptTemplate, externalUrl))
+			fmt.Sprintf(templates.OntologyConceptTemplate, concept.Conceptar, externalUrl))
 
-		err = os.WriteFile(fmt.Sprintf("%s/%s", workingDir, concept), content, 0660)
+		err = os.WriteFile(fmt.Sprintf("%s/%s/%s.md", workingDir, concept.Path, concept.ConceptEn), content, 0660)
 		if err != nil {
 			fmt.Println(err)
 		}
